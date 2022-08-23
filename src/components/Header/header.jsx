8bdbd5eb8487cbe02c764903c8bf3adf06983bae
EuMotta <Hyperxmotta@hotmail.com>
2022-08-23 10:09:00 -0300

    Header Navbar

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import './header.css'
 
+const nav_links = [
+  {
+    path: "#home",
+    display: "Home",
+  },
+  {
+    path: "#about",
+    display: "About",
+  },
+  {
+    path: "#services",
+    display: "Services",
+  },
+  {
+    path: "#projects",
+    display: "Contato",
+  },
+  {
+    path: "#contact",
+    display: "Contact",
+  }
+]
 const header = () => {
   return (
-   <header className='header'>.h</header>
+    <header className='header'>
+      <div className="container">
+        <nav className="nav_bar">
+          <div className="logo"><h2>José Antonio</h2></div>
+          {/* Barra de navegação */}
+          <div className="navigation">
+            <ul className="menu">
+              {
+                nav_links.map((item, index) => (
+                  <li className="menu_item">
+                    <a href={item.path} className="menu_index">
+                      {item.display}
+                    </a>
+                  </li>
+                ))}
+            </ul>
+          </div>
+          {/* lightmode */}
+          <div className="light_mode">
+            <span>
+            <i class="ri-sun-line"></i>LightMode
+            </span>
+          </div>
+          {/* login */}
+        </nav>
+      </div>
+    </header>
+
   )
 }
 
